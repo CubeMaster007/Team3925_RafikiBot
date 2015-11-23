a88b51fff500747ead10e7dec5322063e654d672
atomicdiamond <atomic.diamond2000@gmail.com>
2015-11-23 09:15:09 -0800

    +added xboxHelper +added elevator state machine foundations +added Constants

---
+++ src/org/usfirst/frc3925/Rafiki/helper/Constants.java
@@ -0,0 +1,13 @@
+package org.usfirst.frc3925.Rafiki.helper;
+
+public final class Constants {
+	private Constants() {}
+	
+	public static final double
+	ELEVATOR_MAX_HEIGHT = 5,
+	ELEVATOR_TOLERANCE = 0.3,
+	XBOX_AXIS_TOLERANCE = 0.1,
+	DRIVETRAIN_TOLERANCE = .3;
+	
+	
+}
---
+++ src/org/usfirst/frc3925/Rafiki/helper/XboxHelper.java
@@ -0,0 +1,50 @@
+package org.usfirst.frc3925.Rafiki.helper;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.usfirst.frc3925.Rafiki.Robot;
+
+import edu.wpi.first.wpilibj.Joystick;
+
+public class XboxHelper {
+	private XboxHelper() {}
+	
+	public static enum CurrentXbox {
+		DRIVER, SHOOTER;
+	}
+	
+	public static final int
+	A = 1,
+	B = 2,
+	X = 3,
+	Y = 4,
+	TRIGGER_LT = 5,
+	TRIGGER_RT = 6,
+	START = 7,
+	BACK = 8,
+	STICK_LEFT = 9,
+	STICK_RIGHT = 10,
+	
+	AXIS_LEFT_X = 1,
+	AXIS_LEFT_Y = 2,
+	AXIS_RIGHT_X = 3,
+	AXIS_RIGHT_Y = 4;
+	
+	
+	private static Map<CurrentXbox, Joystick> controllers = new HashMap<>();
+	
+	public static void init() {
+		controllers.put(CurrentXbox.DRIVER, Robot.oi.driver);
+		controllers.put(CurrentXbox.SHOOTER, Robot.oi.shooter);
+	}
+	
+	public static double getAxis(CurrentXbox xbox, int axis) {
+		return Math.abs(controllers.get(xbox).getRawAxis(axis)) > Constants.XBOX_AXIS_TOLERANCE ? controllers.get(xbox).getRawAxis(axis) : 0;
+	}
+	
+	public static double getButton(CurrentXbox xbox, int button) {
+		return controllers.get(xbox).getRawAxis(button);
+	}
+	
+}
